159f794eb66b7bdb4b61ef9ee26ccdcf6dd7edf8
rossyvee <roseakinyi001@gmail.com>
2022-06-11 13:49:09 +0300

    creating the signup form and an email field

--- wins/forms.py
+++ wins/forms.py
@@ -1,1 +1,8 @@
 from django import forms
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
