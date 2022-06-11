428bfa39a198ed27de4d9ce28d7fe404fd58c635
rossyvee <roseakinyi001@gmail.com>
2022-06-11 13:09:10 +0300

    creating the ratings table and the ratings from 1-10

--- wins/models.py
+++ wins/models.py
@@ -36,3 +36,17 @@ class Post(models.Model):
 
     def save_post(self):
             self.save()
+
+class Rating(models.Model):
+    rating = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )
