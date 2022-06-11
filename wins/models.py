255893678c3f138d27afdd3dbe293ba62c874b77
rossyvee <roseakinyi001@gmail.com>
2022-06-11 12:34:46 +0300

    creating the user profile

--- wins/models.py
+++ wins/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = CloudinaryField('images')
+    bio = models.TextField(max_length=500, default="This is my story,this is my life,it has been intresting", blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    location = models.CharField(max_length=60, blank=True)
+    contact = models.EmailField
