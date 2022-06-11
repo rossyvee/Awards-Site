56f36ea7282d834f50d795498f23a85667a774ad
rossyvee <roseakinyi001@gmail.com>
2022-06-11 12:41:55 +0300

    updating the profile model by adding the profile-picture

--- wins/models.py
+++ wins/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from cloudinary.models import CloudinaryField
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     profile_picture = CloudinaryField('images')
