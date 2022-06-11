4b8023f5921c1e9a4e301e353608559ce637238e
rossyvee <roseakinyi001@gmail.com>
2022-06-11 14:13:00 +0300

    creating the class for user serializer

--- wins/serializers.py
+++ wins/serializers.py
@@ -11,3 +11,12 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'url', 'description', 'technologies', 'photo', 'date', 'user']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+    posts = PostSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'url', 'username', 'profile', 'posts']
