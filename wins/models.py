1b17eb83f63db970337dee580ed8db22fe66c366
rossyvee <roseakinyi001@gmail.com>
2022-06-11 14:45:08 +0300

    updating the model with decorators

--- wins/models.py
+++ wins/models.py
@@ -1,16 +1,32 @@
 from django.db import models
+from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from cloudinary.models import CloudinaryField
+import datetime as dt
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     profile_picture = CloudinaryField('images')
     bio = models.TextField(max_length=500, default="This is my story,this is my life,it has been intresting", blank=True)
     name = models.CharField(blank=True, max_length=120)
     location = models.CharField(max_length=60, blank=True)
-    contact = models.EmailField
+    contact = models.EmailField(max_length=100, blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
 
 class Post(models.Model):
     title = models.CharField(max_length=155)
@@ -21,21 +37,25 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     date = models.DateTimeField(auto_now_add=True, blank=True)
 
-
     def __str__(self):
         return f'{self.title}'
 
     def delete_post(self):
         self.delete()
 
+    @classmethod
     def search_project(cls, title):
         return cls.objects.filter(title__icontains=title).all()
 
+    @classmethod
     def all_posts(cls):
         return cls.objects.all()
 
     def save_post(self):
-            self.save()
+        self.save()
+
+
+
 
 class Rating(models.Model):
     rating = (
@@ -50,6 +70,7 @@ class Rating(models.Model):
         (9, '9'),
         (10, '10'),
     )
+
     design = models.IntegerField(choices=rating, default=0, blank=True)
     usability = models.IntegerField(choices=rating, blank=True)
     content = models.IntegerField(choices=rating, blank=True)
@@ -63,11 +84,12 @@ class Rating(models.Model):
     def save_rating(self):
         self.save()
 
+    @classmethod
     def get_ratings(cls, id):
         ratings = Rating.objects.filter(post_id=id).all()
         return ratings
 
-
     def __str__(self):
         return f'{self.post} Rating'
+
 
