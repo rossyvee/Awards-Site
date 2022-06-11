b71d604cb5c7423961d4f33d4110fa81130f2fde
rossyvee <roseakinyi001@gmail.com>
2022-06-11 14:10:42 +0300

    creating the class post serializer

--- wins/serializers.py
+++ wins/serializers.py
@@ -1,5 +1,13 @@
+from rest_framework import serializers
+from .models import Profile, Post
+from django.contrib.auth.models import User
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['name', 'profile_picture', 'bio', 'location', 'contact']
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'url', 'description', 'technologies', 'photo', 'date', 'user']
