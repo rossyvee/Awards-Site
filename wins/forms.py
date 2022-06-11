f4bc650676250cb72cab7389811e78acc9bea3ae
rossyvee <roseakinyi001@gmail.com>
2022-06-11 13:58:17 +0300

    updating imports to the module

--- wins/forms.py
+++ wins/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Post, Profile, Rating
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
